e4888708ba72e30be3efdff4167f6b12445522b0
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2024-08-29 00:44:41 -0500

    add like counter

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -1,21 +1,22 @@
+import { Likes } from '~/components/Likes';
+
 export default function Home() {
   return (
-    <main class="container mx-auto">
-      <div class="text-center py-20">
-        <div class="text-4xl">Audio Normalization Demo</div>
-      </div>
-      <div>
-        <video
-          class="mx-auto"
-          preload="auto"
-          // autoplay
-          controls
-          width="600"
-          poster="https://assets.codepen.io/32795/poster.png"
-        >
-          <source id="mp4" src="http://media.w3.org/2010/05/sintel/trailer.mp4" type="video/mp4" />
-        </video>
-      </div>
+    <main class="container mx-auto flex justify-center items-center flex-col py-16 space-y-16">
+      <div class="text-4xl">Audio Normalization Demo</div>
+
+      <video
+        class="mx-auto"
+        preload="auto"
+        // autoplay
+        controls
+        width="600"
+        poster="https://assets.codepen.io/32795/poster.png"
+      >
+        <source id="mp4" src="http://media.w3.org/2010/05/sintel/trailer.mp4" type="video/mp4" />
+      </video>
+
+      <Likes />
     </main>
   );
 }
